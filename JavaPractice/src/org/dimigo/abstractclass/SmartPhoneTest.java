fdc188336f897a4f57fe38c51abf12eab782c836
Chanjin <Chanjin@PCJ>
2015-09-01 10:16:40 +0900

    추상클래스로 업그레이드

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,22 @@
+package org.dimigo.abstractclass;
+
+public class SmartPhoneTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SmartPhone[] Phones = {
+				new IPhone("iPhone6", "애플", 700000),
+				new Galaxy("갤럭시 S6", "삼성", 650000)
+		};
+		for(SmartPhone p : Phones){
+			System.out.println(p.toString());
+			p.useSpecialFunction();
+			p.turnOn();
+			p.pay();
+			p.turnOff();
+			
+		}
+	}
+
+}
+ 
